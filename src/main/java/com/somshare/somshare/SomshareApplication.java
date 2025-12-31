59d23ca7559f6a9d91cdd7987b0a5df5165dc99d
gaeunee2 <gaeunee2>
2025-12-31 17:47:26 +0900

    init: Spring Boot backend project setup

---
+++ src/main/java/com/somshare/somshare/SomshareApplication.java
@@ -0,0 +1,13 @@
+package com.somshare.somshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somshare/somshare/SomshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somshare.somshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
